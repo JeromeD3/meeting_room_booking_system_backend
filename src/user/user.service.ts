601ea7f23560299b5fd01a1f56b4d6800fe7dfa5
邓祥柏 <1760399176@qq.com>
2024-01-06 23:08:22 +0800

    feat: 注入user实体

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,15 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm/repository/Repository';
+import { RegisterUserDto } from './dto/ register-user.dto';
+import { User } from './entities/User.entity';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  private logger = new Logger();
+
+  @InjectRepository(User)
+  private userRepository: Repository<User>;
+
+  async register(user: RegisterUserDto) {}
+}
