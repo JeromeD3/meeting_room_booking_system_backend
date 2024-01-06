0f43c08a23e6c44fb039a126395b8a33c6b55bcd
邓祥柏 <1760399176@qq.com>
2024-01-06 22:35:59 +0800

    feat: 新增对参数的校验

--- src/user/dto/ register-user.dto.ts
+++ src/user/dto/ register-user.dto.ts
@@ -1,12 +1,38 @@
 //dto 是封装 body 里的请求参数的
+import { IsEmail, IsNotEmpty, MinLength } from 'class-validator';
+
 export class RegisterUserDto {
+  @IsNotEmpty({
+    message: '用户名不能为空',
+  })
   username: string;
 
+  @IsNotEmpty({
+    message: '昵称不能为空',
+  })
   nickName: string;
 
+  @IsNotEmpty({
+    message: '密码不能为空',
+  })
+  @MinLength(6, {
+    message: '密码不能少于 6 位',
+  })
   password: string;
 
+  @IsNotEmpty({
+    message: '邮箱不能为空',
+  })
+  @IsEmail(
+    {},
+    {
+      message: '不是合法的邮箱格式',
+    },
+  )
   email: string;
 
-  captcha: string; // 验证码
+  @IsNotEmpty({
+    message: '验证码不能为空',
+  })
+  captcha: string;
 }
