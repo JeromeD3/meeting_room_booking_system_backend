0c989cc95866a25925fa1ddae8fdb8768221f311
邓祥柏 <1760399176@qq.com>
2024-01-21 15:46:06 +0800

    feat: 新增登录鉴权装饰器

---
+++ src/login.guard.ts
@@ -0,0 +1,71 @@
+import {
+  CanActivate,
+  ExecutionContext,
+  Inject,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+import { JwtService } from '@nestjs/jwt';
+import { Request } from 'express';
+import { Observable } from 'rxjs';
+import { Permission } from './user/entities/permission.entity';
+
+interface JwtUserData {
+  userId: number;
+  username: string;
+  roles: string[];
+  permissions: Permission[];
+}
+
+declare module 'express' {
+  interface Request {
+    user: JwtUserData;
+  }
+}
+@Injectable()
+export class LoginGuard implements CanActivate {
+  @Inject()
+  private reflector: Reflector;
+
+  @Inject(JwtService)
+  private jwtService: JwtService;
+
+  canActivate(
+    context: ExecutionContext,
+  ): boolean | Promise<boolean> | Observable<boolean> {
+    const request: Request = context.switchToHttp().getRequest(); //获取http请求信息
+
+    const requireLogin = this.reflector.getAllAndOverride('require-login', [
+      context.getClass(), //校验整个class
+      context.getHandler(), // 校验每个方法
+    ]);
+
+    if (!requireLogin) {
+      return true;
+    }
+
+    const authorization = request.headers.authorization;
+
+    if (!authorization) {
+      throw new UnauthorizedException('用户未登录');
+    }
+
+    try {
+      const token = authorization.split(' ')[1]; // 获取token
+
+      const data = this.jwtService.verify<JwtUserData>(token); //验证token是否过期
+
+      // 通过，将用户信息存到全局request中
+      request.user = {
+        userId: data.userId,
+        username: data.username,
+        roles: data.roles,
+        permissions: data.permissions,
+      };
+      return true;
+    } catch (e) {
+      throw new UnauthorizedException('token 失效，请重新登录');
+    }
+  }
+}
